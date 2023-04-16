6345c7723638b47fb3af27c7d0a5ac4004e50d65
nomanajaz65 <nomanajaz65@gmail.com>
2023-04-16 06:20:50 -0700

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+function foo(a){
+var t =document.getElementById('inp')
+t.value += a
+}
+
+
+function res(){
+    var a = document.getElementById('inp')
+    a.value= eval (a.value)
+}
+
+function clr(){  
+    document.getElementById('inp').value = ''
+}
