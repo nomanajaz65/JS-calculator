5fad9433ba00640e8c9fd4dd88062f04febd6933
nomanajaz65 <nomanajaz65@gmail.com>
2024-11-23 08:23:23 -0800

    first comment

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-function foo(a){
-var t =document.getElementById('inp')
-t.value += a
+function foo(a) {
+    var t = document.getElementById('inp');
+    t.value += a;
 }
 
-
-function res(){
-    var a = document.getElementById('inp')
-    a.value= eval (a.value)
+function res() {
+    var a = document.getElementById('inp');
+    a.value = eval(a.value);
 }
 
-function clr(){  
-    document.getElementById('inp').value = ''
+function clr() {
+    document.getElementById('inp').value = '';
 }
